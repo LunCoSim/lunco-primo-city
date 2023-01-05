8dede844e0517872973c93259c69c378b83da398
Difint <yarm-public@protonmail.com>
2023-01-05 23:17:51 +0700

    - Updates

--- Primum/buildings/LivingModule.gd
+++ Primum/buildings/LivingModule.gd
@@ -1,16 +1,15 @@
 tool
 extends Building
-class_name SpacePort
+class_name LivingModule
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	type = "SpacePort"
+	type = "LivingModule"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
